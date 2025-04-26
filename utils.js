b42ccb2007e5595bbfe02924b3e855f1746b15cf
aman-james <amanjames122@gmail.com>
2025-04-26 03:56:35 -0700

    added functions for checking name and roomid

--- utils.js
+++ utils.js
@@ -1,7 +1,21 @@
-export function randomStr(len, arr) {
+function randomStr(len, arr) {
   let ans = "";
+  ans.length;
   for (let i = len; i > 0; i--) {
     ans += arr[Math.floor(Math.random() * arr.length)];
   }
   console.log(ans);
 }
+
+function checkRoomId(id) {
+  return id?.length === 10;
+}
+
+function checkName(name) {
+  return name?.length >= 3;
+}
+
+module.exports.checkName = checkName;
+module.exports.checkRoomId = checkRoomId;
+
+module.exports.randomStr = randomStr;
